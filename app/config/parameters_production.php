096d51a7dece25ff803a95521c82b02d8c4c1bce
chebbi.yosri@gmail.com <chebbi.yosri@gmail.com>
2017-11-02 15:29:24 +0100

    make it better

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -1,12 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 $db = parse_url(getenv('CLEARDB_DATABASE_URL'));
 
 $container->setParameter('database_driver', 'pdo_mysql');
